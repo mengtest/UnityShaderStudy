2a91f7f60df209f695deb5e73676f303e95d9cc5
huangxin <ihuangx@outlook.com>
2020-02-15 16:20:50 +0800

    运动模糊简单实现

---
+++ UnityProj/Assets/UnityShaderStudy/Screen Post-processing Effects/MotionBlur.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionBlur : PostEffectsBase
+{
+    public Shader motionBlurShader;
+    private Material motionBlurMaterial = null;
+    public Material material {
+        get {
+            motionBlurMaterial = CheckShaderAndCreateMaterial(motionBlurShader, motionBlurMaterial);
+            return motionBlurMaterial;
+        }
+    }
+
+    [Range(0.0f, 0.9f)]
+    public float blurAmount = 0.5f; // 值越大，运动拖尾效果就越明显
+
+    private RenderTexture accumulationTexture;
+
+    void OnDisable()
+    {
+        DestroyImmediate(accumulationTexture);
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null) {
+            if (accumulationTexture == null || accumulationTexture.width != src.width || 
+                accumulationTexture.height != src.height) {
+                DestroyImmediate(accumulationTexture);
+                accumulationTexture = new RenderTexture(src.width, src.height, 0);
+                accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(src, accumulationTexture);
+            }
+
+            accumulationTexture.MarkRestoreExpected();
+
+            material.SetFloat("_BlurAmount", 1.0f - blurAmount);
+
+            Graphics.Blit(src, accumulationTexture, material, 0);
+            Graphics.Blit(accumulationTexture, dest);
+        }
+        else {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
